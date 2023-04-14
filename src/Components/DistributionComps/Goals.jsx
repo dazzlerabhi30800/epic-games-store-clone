1a1be42dfc02268fe64d38e9a9befd594a29a3bd
dazzlerabhi30800 <abhisingh30800@gmail.com>
2023-04-14 20:42:40 +0530

    added goals comp

---
+++ src/Components/DistributionComps/Goals.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+import data from "./GoalsData";
+
+const Goals = () => {
+  return (
+    <>
+      <div className="goals--container grid grid-cols-1 lg:grid-cols-3 my-6 gap-8 lg:gap-14">
+        {data.map((item, index) => {
+          return (
+            <div
+              key={index}
+              className="goals--card font-poppins flex flex-col text-center items-center border border-gray-500 p-4 rounded-md gap-5 justify-between"
+            >
+              <img className="w-32 h-32" src={item.image} alt={item.heading} />
+              <h2 className="text-2xl lg:text-3xl">{item.heading}</h2>
+              <p className="text-sm lg:text-base text-gray-300">{item.desc}</p>
+            </div>
+          );
+        })}
+      </div>
+    </>
+  );
+};
+
+export default Goals;
---
+++ src/Components/DistributionComps/GoalsData.jsx
@@ -0,0 +1,32 @@
+export default [
+  {
+    image: "./imgs/distribution-imgs/global.svg",
+    heading: "Reach a Global Audience",
+    desc: "Direct distribution to over 230 million Epic users across 187 countries with 16 languges supported.",
+  },
+  {
+    image: "./imgs/distribution-imgs/revenue.svg",
+    heading: "88%/12% Revenue Split",
+    desc: "Keep 88% of the revenue and monitor product performance with integrated analytics dashboards.",
+  },
+  {
+    image: "./imgs/distribution-imgs/engagement.svg",
+    heading: "Driver Player Engagement",
+    desc: "Tap into store features like wishlists, achievements, store-wide promotions and more!",
+  },
+  {
+    image: "./imgs/distribution-imgs/e-commerce.svg",
+    heading: "Worldwide E-Commerce",
+    desc: "Epic's payment service supports 76 payment methods with 47 regional currencies and more on the way.",
+  },
+  {
+    image: "./imgs/distribution-imgs/wallet.svg",
+    heading: "Epic Wallet",
+    desc: "Users can load up their Wallet with funds to spend on products and services in the store, now available in more than 140 countries.",
+  },
+  {
+    image: "./imgs/distribution-imgs/ratings.svg",
+    heading: "Additional Benefits",
+    desc: "Easy IARC ratings in Epic Developer Portal, request no-cost localization for store pages and activate our Support-A-Creator affiliate network.",
+  },
+];
